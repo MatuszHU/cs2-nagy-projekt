3e315fdc5a9c261eed9713f416a13fd7ee8112b0
Majoros Máté <matusz@mailbox.unideb.hu>
2025-09-29 18:45:39 +0200

    Init

---
+++ main.lua
@@ -0,0 +1,11 @@
+function love.load()
+    love.window.setTitle("CS2 Nagy Projekt")
+end
+
+function love.update(dt)
+
+end
+
+function love.draw()
+    love.graphics.print('Helló világ!', 400, 300)
+end
