1746f25b653ad9b6697e05bda1add3c44e4042ac
Bencezs1 <zsbence2003@gmail.com>
2025-10-09 13:23:45 +0200

    add: Initial race settings

---
+++ race.lua
@@ -0,0 +1,78 @@
+return {
+    orc = {
+        name = "Orc",
+        stats = {
+            attack = 25,
+            defense = 20,
+            magic = 4,
+            resistance = 2,
+            accuracy = 12,
+            evasion = 10,
+            luck = 1,
+            fow = 1,
+            movement = 3
+        },
+        image = ""
+    },
+    elf = {
+        name = "Elf",
+        stats = {
+            attack = 10,
+            defense = 12,
+            magic = 10,
+            resistance = 18,
+            accuracy = 25,
+            evasion = 13,
+            luck = 3,
+            fow = 4,
+            movement = 7
+        },
+        image = ""
+    },
+    goblin = {
+        name = "Goblin",
+        stats = {
+            
+            attack = 3,
+            defense = 4,
+            magic = 7,
+            resistance = 10,
+            accuracy = 10,
+            evasion = 25,
+            luck = 7,
+            fow = 2,
+            movement = 7
+        },
+        image = ""
+    },
+    human = {
+        name = "Human",
+        stats = {  
+            attack = 10,
+            defense = 10,
+            magic = 2,
+            resistance = 10,
+            accuracy = 15,
+            evasion = 20,
+            luck = 2,
+            fow = 4,
+            movement = 5
+        },
+        image = ""
+    },
+    dwarf = {
+        name = "Dwarf",
+        stats = {
+            attack = 25,
+            defense = 12,
+            magic = 1,
+            resistance = 2,
+            accuracy = 13,
+            evasion = 12,
+            luck = 2,
+            fow = 2,
+            movement = 3
+        },
+        image = ""
+    }
+}
