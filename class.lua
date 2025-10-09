8209a24404253033e3593d287457daa8e88b8b78
creativeusername6 <almos2002@mailbox.unideb.hu>
2025-10-09 13:49:23 +0200

    add: Initial class settings

---
+++ class.lua
@@ -0,0 +1,50 @@
+return {
+    knight = {
+        name = "knight",
+        stats = {
+            attack = 7,
+            defense = 2,
+            resistance = 2,
+            accuracy = 2
+        }
+    },
+    cavalry = {
+        name = "cavalry",
+        stats = {
+            attack = 3,
+            defense = 7,
+            resistance = 7,
+            evasion = 5,
+            fow = 2,
+            movement = 4
+        }
+    },
+    wizard = {
+        name = "wizard",
+        stats = {
+            magic = 6,
+            resistance = 3,
+            accuracy = 4
+        }
+    },
+    priest = {
+        name = "priest",
+        stats = {
+            defense = 5,
+            magic = 1,
+            resistance = 5,
+            accuracy = 2,
+            evasion = 5,
+            luck = 1,
+            movement = 1
+        }
+    },
+    thief = {
+        name = "thief",
+        stats = {
+            luck = 10,
+            fow = 2,
+            movement = 3
+        }
+    }
+}
