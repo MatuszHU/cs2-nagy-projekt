e5d3de1ee8624b98bc506acb2437d81091201170
Majoros Máté <matusz@mailbox.unideb.hu>
2025-10-13 14:16:05 +0200

    Fixing mouseHover

--- Button.lua
+++ Button.lua
@@ -27,7 +27,7 @@ function Button(text, func, func_param, width, height)
                 end
             end
         end,
-        mouseHover = function(mouse_x, mouse_y, cursor_radius)
+        mouseHover = function(self, mouse_x, mouse_y, cursor_radius)
             if (mouse_x + cursor_radius >= self.button_x) and (mouse_x - cursor_radius <= self.button_x + self.width) then
                 if (mouse_y + cursor_radius >= self.button_y) and (mouse_y - cursor_radius <= self.button_y + self.height) then
                     return true
